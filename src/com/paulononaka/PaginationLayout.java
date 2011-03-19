4f7332212ea530f6281e5389393adcca846bdaf6
Paulo Henrique Nonaka <paulononaka@gmail.com>
2011-03-18 23:52:30 -0300

    change lincenses

--- src/com/paulononaka/PaginationLayout.java
+++ src/com/paulononaka/PaginationLayout.java
@@ -1,17 +1,18 @@
 /*
- * Copyright (C) 2011, Paulo Henrique Nonaka - paulononaka@gmail.com
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (C) 2011,  Paulo Henrique Nonaka - paulononaka@gmail.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.paulononaka;
@@ -38,138 +39,134 @@ import android.widget.LinearLayout;
  */
 public class PaginationLayout extends LinearLayout {
 
-    private int mPageActive = 0;
-    private HorizontalScrollView mScroll;
-    private LinearLayout mBar;
+	private int mPageActive = 0;
+	private HorizontalScrollView mScroll;
+	private LinearLayout mBar;
 
-    public PaginationLayout(Context context) {
-	super(context);
+	public PaginationLayout(Context context) {
+		super(context);
 
-	setOrientation(LinearLayout.VERTICAL);
+		setOrientation(LinearLayout.VERTICAL);
 
-	// creates the class that will control the gestures and apply it in the scroll
-	final GestureDetector mGestureDetector = new GestureDetector(
-		new MySimpleOnGestureListener());
+		// creates the class that will control the gestures and apply it in the
+		// scroll
+		final GestureDetector mGestureDetector = new GestureDetector(new MySimpleOnGestureListener());
 
-	mScroll = new HorizontalScrollView(context);
-	mScroll.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
-		LayoutParams.FILL_PARENT));
-	mScroll.setOnTouchListener(new View.OnTouchListener() {
+		mScroll = new HorizontalScrollView(context);
+		mScroll.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+		mScroll.setOnTouchListener(new View.OnTouchListener() {
 
-	    public boolean onTouch(View v, MotionEvent event) {
-		if (mGestureDetector.onTouchEvent(event)) {
-		    return true;
-		} else {
-		    return false;
-		}
-	    }
-	});
+			public boolean onTouch(View v, MotionEvent event) {
+				if (mGestureDetector.onTouchEvent(event)) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		});
 
-	// creates Previous and Next buttons
-	Button btnPrevious = new Button(getContext());
-	btnPrevious.setLayoutParams(new LayoutParams(150,
-		LayoutParams.FILL_PARENT));
-	btnPrevious.setText("Previous");
-	btnPrevious.setOnClickListener(new OnClickListener() {
+		// creates Previous and Next buttons
+		Button btnPrevious = new Button(getContext());
+		btnPrevious.setLayoutParams(new LayoutParams(150, LayoutParams.FILL_PARENT));
+		btnPrevious.setText("Previous");
+		btnPrevious.setOnClickListener(new OnClickListener() {
 
-	    public void onClick(View v) {
-		previous();
-	    }
-	});
+			public void onClick(View v) {
+				previous();
+			}
+		});
 
-	Button btnNext = new Button(getContext());
-	btnNext.setLayoutParams(new LayoutParams(150, LayoutParams.FILL_PARENT));
-	btnNext.setText("Next");
-	btnNext.setOnClickListener(new OnClickListener() {
+		Button btnNext = new Button(getContext());
+		btnNext.setLayoutParams(new LayoutParams(150, LayoutParams.FILL_PARENT));
+		btnNext.setText("Next");
+		btnNext.setOnClickListener(new OnClickListener() {
 
-	    public void onClick(View v) {
-		next();
-	    }
-	});
+			public void onClick(View v) {
+				next();
+			}
+		});
 
-	// bar that include the buttons
-	mBar = new LinearLayout(getContext());
-	mBar.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
-		LayoutParams.WRAP_CONTENT));
-	mBar.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
-	mBar.setBackgroundColor(Color.GRAY);
-	mBar.addView(btnPrevious);
-	mBar.addView(btnNext);
-	mBar.setVisibility(LinearLayout.INVISIBLE);
-
-	// add bar and scroll in the super (LinearLayout)
-	super.addView(mBar);
-	super.addView(mScroll);
-    }
-
-    /**
-     * All that the user include is added in the scroll
-     */
-    @Override
-    public void addView(View child) {
-	mScroll.addView(child);
-    }
-
-    /**
-     * Controls if the top bar should appear or not
-     */
-    @Override
-    protected void onSizeChanged(int arg0, int arg1, int arg2, int arg3) {
-	super.onSizeChanged(arg0, arg1, arg2, arg3);
-	View chield = mScroll.getChildAt(0);
-	if (chield != null) {
-	    if (chield.getMeasuredWidth() > getWidth()) {
-		mBar.setVisibility(LinearLayout.VISIBLE);
-	    } else {
+		// bar that include the buttons
+		mBar = new LinearLayout(getContext());
+		mBar.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+		mBar.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
+		mBar.setBackgroundColor(Color.GRAY);
+		mBar.addView(btnPrevious);
+		mBar.addView(btnNext);
 		mBar.setVisibility(LinearLayout.INVISIBLE);
-	    }
+
+		// add bar and scroll in the super (LinearLayout)
+		super.addView(mBar);
+		super.addView(mScroll);
 	}
-    }
 
-    /**
-     * does the effect "back a page"
-     */
-    public void previous() {
-	mPageActive = (mPageActive > 0) ? mPageActive - 1 : 0;
-	mScroll.smoothScrollTo(mPageActive * mScroll.getWidth(), 0);
-    }
-
-    /**
-     * does the effect "forward a page"
-     */
-    public void next() {
-	int pageWidth = mScroll.getWidth();
-	int nextPage = (mPageActive + 1) * pageWidth;
-	if (nextPage - mScroll.getScrollX() <= pageWidth) {
-	    mScroll.smoothScrollTo(nextPage, 0);
-	    mPageActive++;
-	} else {
-	    mScroll.smoothScrollTo(mScroll.getScrollX(), 0);
-	}
-    }
-
-    /**
-     * Private class that controls the gestures, forward or back a page.
-     */
-    private class MySimpleOnGestureListener extends SimpleOnGestureListener {
-
+	/**
+	 * All that the user include is added in the scroll
+	 */
 	@Override
-	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-		float velocityY) {
-
-	    if (e1 != null && e2 != null) {
-		if (e1.getX() - e2.getX() > 0) {
-		    // forward...
-		    next();
-		    return true;
-		} else if (e2.getX() - e1.getX() > 0) {
-		    // back...
-		    previous();
-		    return true;
-		}
-	    }
-
-	    return false;
+	public void addView(View child) {
+		mScroll.addView(child);
 	}
-    }
+
+	/**
+	 * Controls if the top bar should appear or not
+	 */
+	@Override
+	protected void onSizeChanged(int arg0, int arg1, int arg2, int arg3) {
+		super.onSizeChanged(arg0, arg1, arg2, arg3);
+		View chield = mScroll.getChildAt(0);
+		if (chield != null) {
+			if (chield.getMeasuredWidth() > getWidth()) {
+				mBar.setVisibility(LinearLayout.VISIBLE);
+			} else {
+				mBar.setVisibility(LinearLayout.INVISIBLE);
+			}
+		}
+	}
+
+	/**
+	 * does the effect "back a page"
+	 */
+	public void previous() {
+		mPageActive = (mPageActive > 0) ? mPageActive - 1 : 0;
+		mScroll.smoothScrollTo(mPageActive * mScroll.getWidth(), 0);
+	}
+
+	/**
+	 * does the effect "forward a page"
+	 */
+	public void next() {
+		int pageWidth = mScroll.getWidth();
+		int nextPage = (mPageActive + 1) * pageWidth;
+		if (nextPage - mScroll.getScrollX() <= pageWidth) {
+			mScroll.smoothScrollTo(nextPage, 0);
+			mPageActive++;
+		} else {
+			mScroll.smoothScrollTo(mScroll.getScrollX(), 0);
+		}
+	}
+
+	/**
+	 * Private class that controls the gestures, forward or back a page.
+	 */
+	private class MySimpleOnGestureListener extends SimpleOnGestureListener {
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+			if (e1 != null && e2 != null) {
+				if (e1.getX() - e2.getX() > 0) {
+					// forward...
+					next();
+					return true;
+				} else if (e2.getX() - e1.getX() > 0) {
+					// back...
+					previous();
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
 }
